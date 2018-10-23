e258f090935f3af249da2edfa3c29c8bec546a6c
Ein <ein.paul@gmail.com>
2018-10-23 04:27:59 -0400

    added about page with routes and action

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,14 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
     public function index()
     {
         return view('home');
+    }
+
+    public function about()
+    {
+        return view('about');
     }
 }
