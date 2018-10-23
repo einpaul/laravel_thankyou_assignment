49b16a7e8a989f761dc40585172186cf43ab4c31
Ein <ein.paul@gmail.com>
2018-10-23 16:35:03 -0400

    added title & body columns to posts

--- database/factories/PosTFactory.php
+++ database/factories/PosTFactory.php
@@ -2,8 +2,11 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Post::class, function (Faker $faker) {
+$factory->define(\App\Post::class, function (Faker $faker) {
     return [
-        //
+
+        'title' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'body'=> $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
     ];
 });
+
